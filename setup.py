1cd60e95ccb79e1450e361e7a3c0bef5a4755e44
Jonathan Cross <jrxFive@gmail.com>
2015-02-11 12:19:52 -0500

    Added setup.py, moved boostrap outside of folder

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="consul-alerting",
+    version="0.0.1",
+    description="A set of python files for Consul for checks, watches, and notifications",
+    url="https://github.com/jrxFive/Consul-Alerting",
+    author="Jonathan R. Cross",
+    author_email="jrxfive@gmail.com",
+    license=open('LICENSE').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+    ],
+    keywords="consul notifications",
+    install_requires=['consulate',
+    'python-simple-hipchat']
+)
