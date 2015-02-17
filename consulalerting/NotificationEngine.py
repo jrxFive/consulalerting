f684b0f959ef037b24a5c22424ec659269f49805
Jonathan Cross <jrxFive@gmail.com>
2015-02-17 17:19:49 -0500

    multiprocessing added to NotificationEngine
    Added multiprocessing for the notify loop, may need to control how many
    processes are spun up. The notifiers api calls take up most of the CPU
    time. Added NotificationEngine-tests

--- consulalerting/NotificationEngine.py
+++ consulalerting/NotificationEngine.py
@@ -5,7 +5,7 @@ import consulate
 import smtplib
 import string
 from slacker import Slacker
-from ConsulHealthStruct import ConsulHealthStruct
+from multiprocessing import Process
 from Settings import Settings
 
 class NotificationEngine(Settings):
@@ -103,28 +103,6 @@ class NotificationEngine(Settings):
         return plugin
 
 
-    def Run(self):
-        self.get_available_plugins()
-        self.get_unique_tags_keys()
-        self.load_plugins_from_tags()
-
-        for obj in self.alert_list:
-            # if hipchat in tags and if hipchat KV information exists, need
-            # better way to confirm existence
-            if "hipchat" in obj.Tags and self.hipchat:
-                self.notify_hipchat(obj,self.message_pattern(obj))
-
-            if "slack" in obj.Tags and self.slack:
-                self.notify_slack(obj,self.message_pattern(obj))
-
-            if "mailgun" in obj.Tags and self.mailgun:
-                self.notify_mailgun(obj,self.message_pattern(obj))
-
-            if "email" in obj.Tags and self.email:
-                self.notify_email(obj,self.message_pattern(obj))
-
-
-
     def common_notifiers(self,obj,kv_tags_dictname,kv_dict):
         keynames = set(kv_dict[kv_tags_dictname].keys())
         obj_tags = set(obj.Tags)
@@ -153,93 +131,131 @@ class NotificationEngine(Settings):
 
         return message_template
 
-    def notify_hipchat(self, obj, message_template):
-        common_hipchat_rooms = self.common_notifiers(obj,"rooms",self.hipchat)
+    def run_notifiers(self,obj):
 
-        # Use hipchat library to use for notifications requires api_token and
-        # url
-        hipster = hipchat.HipChat(
-            token=self.hipchat["api_token"], url=self.hipchat["url"])
+        message_template = self.message_pattern(obj)
 
-        for roomname in common_hipchat_rooms:
-
-            if obj.Status == NotificationEngine.PASSING_STATE:
-                color_value = "green"
-                notify_value = 0
-
-            elif obj.Status == NotificationEngine.WARNING_STATE:
-                color_value = "yellow"
-                notify_value = 1
-
-            elif obj.Status == NotificationEngine.CRITICAL_STATE:
-                color_value = "red"
-                notify_value = 1
-
-            elif obj.Status == NotificationEngine.UNKNOWN_STATE:
-                color_value = "gray"
-                notify_value = 1
+        if "hipchat" in obj.Tags and self.hipchat:
+            common_notifiers = self.common_notifiers(obj,"rooms",self.hipchat)
+            hipchat = self.hipchat
+            Process(target=notify_hipchat,args=(obj,message_template,common_notifiers,hipchat)).start()
 
 
-            hipster.message_room(room_id=int(self.hipchat["rooms"][roomname]), message_from="Consul",
-                                 message=message_template,
-                                 notify=notify_value,
-                                 color=color_value)
-
-    def notify_slack(self, obj, message_template):
-        common_slack_rooms = self.common_notifiers(obj,"rooms",self.slack)
+        if "slack" in obj.Tags and self.slack:
+            common_notifiers = self.common_notifiers(obj,"rooms",self.slack)
+            slack =self.slack
+            Process(target=notify_slack,args=(obj,message_template,common_notifiers,slack)).start()
 
 
-        # Use slack library to use for notifications requires api_token and
-        # url
-        slacker = Slacker(self.slack["api_token"])
-
-        for roomname in common_slack_rooms:
-
-            slacker.chat.post_message(self.slack["rooms"][roomname],message_template,"Consul")
+        if "mailgun" in obj.Tags and self.mailgun:
+            common_notifiers = self.common_notifiers(obj,"teams",self.mailgun)
+            mailgun = self.mailgun
+            Process(target=notify_mailgun,args=(obj,message_template,common_notifiers,mailgun)).start()
 
 
-    def notify_mailgun(self,obj, message_template):
-        common_mailgun_teams = self.common_notifiers(obj,"teams",self.mailgun)
-
-
-        api_endpoint = "https://api.mailgun.net/v2/{domain}/messages".format(domain=self.mailgun["mailgun_domain"])
-        auth_tuple=('api', self.mailgun["api_token"])
-
-        for teamname in common_mailgun_teams:
-
-            requests.post(api_endpoint,
-                          auth=auth_tuple,
-                          data={'from':self.mailgun["from"],
-                                'to':self.mailgun["teams"][teamname],
-                                'subject': 'Consul Alert',
-                                'text': message_template})
+        if "email" in obj.Tags and self.email:
+            common_notifiers = self.common_notifiers(obj,"teams",self.email)
+            email = self.email
+            Process(target=notify_email,args=(obj,message_template,common_notifiers,email)).start()
 
 
 
-    def notify_email(self,obj,message_template):
-        common_email_teams = self.common_notifiers(obj,"teams",self.email)
+    def Run(self):
+        self.get_available_plugins()
+        self.get_unique_tags_keys()
+        self.load_plugins_from_tags()
 
-        server = smtplib.SMTP(self.email["mail_domain_address"])
-
-        if self.email["username"] and self.email["password"]:
-            server.login(self.email["username"],self.email["password"])
-
-        from_address = self.email["from"]
-        subject = "Consul Alert"
-
-        for teamname in common_email_teams:
-
-            body = string.join((
-                "From: %s" % from_address,
-                "To: %s" % ', '.join(teamname),
-                "Subject: %s" % subject ,
-                "",
-                message_template
-                ), "\r\n")
-
-            server.sendmail(from_address,teamname,body)
-
-        server.quit()
+        for obj in self.alert_list:
+            self.run_notifiers(obj)
 
 
 
+
+
+def notify_hipchat(obj, message_template,common_notifiers,consul_hipchat):
+    #common_hipchat_rooms = self.common_notifiers(obj,"rooms",self.hipchat)
+
+    # Use hipchat library to use for notifications requires api_token and
+    # url
+    hipster = hipchat.HipChat(
+        token=consul_hipchat["api_token"], url=consul_hipchat["url"])
+
+    for roomname in common_notifiers:
+
+        if obj.Status == NotificationEngine.PASSING_STATE:
+            color_value = "green"
+            notify_value = 0
+
+        elif obj.Status == NotificationEngine.WARNING_STATE:
+            color_value = "yellow"
+            notify_value = 1
+
+        elif obj.Status == NotificationEngine.CRITICAL_STATE:
+            color_value = "red"
+            notify_value = 1
+
+        elif obj.Status == NotificationEngine.UNKNOWN_STATE:
+            color_value = "gray"
+            notify_value = 1
+
+
+        hipster.message_room(room_id=int(consul_hipchat["rooms"][roomname]), message_from="Consul",
+                             message=message_template,
+                             notify=notify_value,
+                             color=color_value)
+
+def notify_slack(obj, message_template,common_notifiers,consul_slack):
+    common_slack_rooms = common_notifiers
+
+
+    # Use slack library to use for notifications requires api_token and
+    # url
+    slacker = Slacker(consul_slack["api_token"])
+
+    for roomname in common_slack_rooms:
+
+        slacker.chat.post_message(consul_slack["rooms"][roomname],message_template,"Consul")
+
+
+def notify_mailgun(obj, message_template,common_notifiers,consul_mailgun):
+    common_mailgun_teams = common_notifiers
+
+
+    api_endpoint = "https://api.mailgun.net/v2/{domain}/messages".format(domain=consul_mailgun["mailgun_domain"])
+    auth_tuple=('api', consul_mailgun["api_token"])
+
+    for teamname in common_mailgun_teams:
+
+        requests.post(api_endpoint,
+                      auth=auth_tuple,
+                      data={'from':consul_mailgun["from"],
+                            'to':consul_mailgun["teams"][teamname],
+                            'subject': 'Consul Alert',
+                            'text': message_template})
+
+
+
+def notify_email(obj,message_template,common_notifiers,consul_email):
+    common_email_teams = common_notifiers
+
+    server = smtplib.SMTP(consul_email["mail_domain_address"])
+
+    if consul_email["username"] and consul_email["password"]:
+        server.login(consul_email["username"],consul_email["password"])
+
+    from_address = consul_email["from"]
+    subject = "Consul Alert"
+
+    for teamname in common_email_teams:
+
+        body = string.join((
+            "From: %s" % from_address,
+            "To: %s" % ', '.join(teamname),
+            "Subject: %s" % subject ,
+            "",
+            message_template
+            ), "\r\n")
+
+        server.sendmail(from_address,teamname,body)
+
+    server.quit()
--- tests/NotificationEngine-tests.py
+++ tests/NotificationEngine-tests.py
@@ -1,11 +1,55 @@
 #!/usr/bin/env python
 import unittest
 import simplejson as json
-import consulalerting.NotificationEngine
-import consulalerting.ConsulHealthStruct
+from consulalerting import NotificationEngine
+from consulalerting import ConsulHealthStruct
 
 
+KV_ALERTING_AVAILABLE_PLUGINS = ["hipchat","slack","mailgun"]
+ALERT_LIST = [{"Node": "consul-agent",
+               "CheckID": "serfHealth",
+               "Name": "Serf Health Status",
+               "Tags": ["devops", "consul","mailgun","hipchat"],
+               "ServiceName": "",
+               "Notes": "",
+               "Status": "critical",
+               "ServiceID": "",
+               "Output": "Agent not live or unreachable"},
+              {"Node": "consul-agent",
+               "CheckID": "service:redis",
+               "Name": "Service 'redis' check",
+               "Tags": ["devops","redis","techops","dev","slack"],
+               "ServiceName": "redis",
+               "Notes": "",
+               "Status": "critical",
+               "ServiceID": "redis",
+               "Output": "Usage: check_redis.py [options]\n\ncheck_redis.py: error: Warning level required\n"},
+              {"Node": "consul",
+               "CheckID": "serfHealth",
+               "Name": "Serf Health Status",
+               "Tags": ["dev", "consul"],
+               "ServiceName": "",
+               "Notes": "",
+               "Status": "passing",
+               "ServiceID": "",
+               "Output": "Agent alive and reachable"},
+              {"Node": "consul",
+               "CheckID": "service:redis",
+               "Name": "Service 'redis' check",
+               "Tags": ["qa", "redis-slave"],
+               "ServiceName": "redis",
+               "Notes": "",
+               "Status": "critical",
+               "ServiceID": "redis",
+               "Output": "Usage: check_redis.py [options]\n\ncheck_redis.py: error: Warning level required\n"}]
 
+CONSUL_HEALTH_STRUCT_ALERT_LIST = [ConsulHealthStruct.ConsulHealthStruct(**obj) for obj in ALERT_LIST]
+
+MAILGUN_NOTIFIER = {"teams":
+                        {"devops":["jrxfive@gmail.com","jonathancr.cross@gmail.com"]},
+                    "from": "Consul@example.com",
+                    "MAILGUN_DOMAIN": "sandbox1b8b7a197a214e778894e5af2f7799a4.mailgun.org",
+                    "API_TOKEN": "key-55028aa08ee351a714e662ccb736dd74"}
 
 class NotificationEngineTests(unittest.TestCase):
 
@@ -13,9 +57,26 @@ class NotificationEngineTests(unittest.TestCase):
 
 
     def setUp(self):
-        self.ne = consulalerting.NotificationEngine()
+        self.ne = NotificationEngine.NotificationEngine(CONSUL_HEALTH_STRUCT_ALERT_LIST)
+        self.ne.available_plugins = KV_ALERTING_AVAILABLE_PLUGINS
+        self.ne.unique_tags = set(["hipchat","mailgun"])
 
 
+    def test_uniqueTags(self):
+        unique_tags = self.ne.get_unique_tags_keys()
+        self.assertEqual(len(unique_tags),len(set(["devops","consul","mailgun","hipchat","redis","techops","dev","slack","qa","redis-slave"])))
+
+
+    def test_loadPluginsFromTags(self):
+        hipchat,slack,mailgun,email = self.ne.load_plugins_from_tags()
+        self.assertFalse(email)
+        self.assertTrue(hipchat)
+        self.assertFalse(slack)
+        self.assertTrue(mailgun)
+
+    def test_commonNotifiers(self):
+        common = self.ne.common_notifiers(CONSUL_HEALTH_STRUCT_ALERT_LIST[0],"teams",MAILGUN_NOTIFIER)
+        self.assertEqual(len(common),1)
 
 
 
@@ -23,4 +84,3 @@ class NotificationEngineTests(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
